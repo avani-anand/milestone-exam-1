420c98f4c45bd2c9e58cafd003d145f3f530062b
Avani Anand <iamavanianand@gmail.com>
2023-10-29 15:50:14 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  // content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
